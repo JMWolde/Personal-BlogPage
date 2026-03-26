7b0641b58a4f67092d9364c72f3fbe2d98cecd2f
JMWolde <jm.wolde@gmail.com>
2026-03-26 19:26:17 -0300

    Redesign: Post/Main Card Layout

--- app/page.tsx
+++ app/page.tsx
@@ -11,18 +11,12 @@ import Card from "../components/Card";
 import {createElement, use, useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
 import {supabase} from "@/lib/supabase";
+import MainCard from "../components/MainCard";
 import {resolveUrl} from "next/dist/lib/metadata/resolvers/resolve-url";
 export default function HomePage() {
   return (
       <main>
-          <div id="MainContainer">
-              <div className="MainCard">
-                  <h1 >JOSH'S CREATIVE DOMAIN</h1>
-                  <p>
-                      Welcome Welcome! This is my blog for all my creative/cs ideas, projects, work, thoughts WHATEVER.
-                      Everything here has been coded by yours truly and still VERY EARLY in development so I hope you enjoy what I've made here!</p>
-              </div>
-          </div>
+         <MainCard/>
           <RetrievePosts/>
 
       </main>
