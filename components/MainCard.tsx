b7a2e74878f83ae205d95519448f03e6e39ea39e
JMWolde <jm.wolde@gmail.com>
2026-03-26 19:31:31 -0300

    Minor fix

---
+++ components/MainCard.tsx
@@ -0,0 +1,14 @@
+
+export default function MainCard(){
+    return (
+        <div id="MainContainer">
+            <div className="MainCard">
+                <h1>JOSH'S CREATIVE DOMAIN</h1>
+                <p>
+                    Welcome Welcome! This is my blog for all my creative/cs ideas, projects, work, thoughts WHATEVER.
+                    Everything here has been coded by yours truly and still VERY EARLY in development so I hope you
+                    enjoy what I've made here!</p>
+            </div>
+        </div>
+    )
+}
