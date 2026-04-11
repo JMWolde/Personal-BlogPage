e459c2acf5189293085a131932930818d1fde921
JMWolde <jm.wolde@gmail.com>
2026-04-11 13:54:47 -0300

    Minor Change: bg video

--- components/bgvideo.tsx
+++ components/bgvideo.tsx
@@ -10,18 +10,18 @@ const videos = [
     // "/assets/videos/bgvideos/IDK_1.mp4",
     // "/assets/videos/bgvideos/JSR_SHIBUYA.mp4",
     // "/assets/videos/bgvideos/KH_ISLAND.mp4",
-    "/assets/videos/bgvideos/M64_CASTLE.mp4",
+    // "/assets/videos/bgvideos/M64_CASTLE.mp4",
     // "/assets/videos/bgvideos/MG_OBSERVATORY.mp4",
     // "/assets/videos/bgvideos/MK8DX_NEOBOWSER.mp4",
     // "/assets/videos/bgvideos/MK_DK.mp4",
-    "/assets/videos/bgvideos/MK_MOOMOO.mp4",
+    // "/assets/videos/bgvideos/MK_MOOMOO.mp4",
     // "/assets/videos/bgvideos/MK_SNOW.mp4",
-    // "/assets/videos/bgvideos/MK_TOADCITY.mp4",
+    "/assets/videos/bgvideos/MK_TOADCITY.mp4",
     // "/assets/videos/bgvideos/MS_IDKSUNSET.mp4",
     // "/assets/videos/bgvideos/SB_BB1.mp4",
     // "/assets/videos/bgvideos/SB_BB2.mp4",
     // "/assets/videos/bgvideos/SB_BB3.mp4",
-    "/assets/videos/bgvideos/WII_RESORT.mp4"
+    // "/assets/videos/bgvideos/WII_RESORT.mp4"
 ]
 
 export default function BgVideo() {
@@ -30,11 +30,11 @@ export default function BgVideo() {
 
     return (
         <>
-            {/*<video autoPlay loop muted src={finalVid} id="bgVideo"/>*/}
-            <img
-                src="/assets/Miscellaneous/yyc_bg2.png"
-                id="bgVideo"
-            />
+            <video autoPlay loop muted src={finalVid} id="bgVideo"/>
+            {/*<img*/}
+            {/*    src="/assets/Miscellaneous/yyc_bg2.png"*/}
+            {/*    id="bgVideo"*/}
+            {/*/>*/}
             <Noise
                 patternSize={250}
                 patternScaleX={10}
