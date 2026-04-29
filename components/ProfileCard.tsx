110434131bbab395b707db3b1e4aed61bf250f89
JMWolde <jm.wolde@gmail.com>
2026-04-28 18:51:21 -0600

    minor profile changes

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -19,10 +19,10 @@ export default function ProfileCard(){
                 <div className="ProfileRow">
                 <div className="ProfileTextContainer">
                     <h2><b>Currently Playing:</b> Super Mario Galaxy & Breath Of The Wild & Tomodachi Life LTD</h2>
-                    <h2><b>Currently Watching:</b> Invincible & 2 Broke Girls</h2>
-                    <h2><b>Song On Repeat:</b> The Blonde - TV Girl</h2>
-                    <h2><b>Status:</b> Health utterly destroyed by energy drinks</h2>
-                    <h2><b>Learning About:</b> The best way to design water in modeling</h2>
+                    <h2><b>Currently Watching:</b> The Boys s5 & 2 Broke Girls</h2>
+                    <h2><b>Song On Repeat:</b> Theme from Superman - John Murphy</h2>
+                    <h2><b>Status:</b> Very excited with all this free time</h2>
+                    <h2><b>Learning:</b> Unity</h2>
                 </div>
                 <img src="/assets/ProfilePics/PicOFMe.jpg"></img>
                 </div>
