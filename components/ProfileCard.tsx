25c70be69efac729d2ae4515511066e1fa5ed4ea
JMWolde <jm.wolde@gmail.com>
2026-05-09 12:43:22 -0600

    minor profile changes

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -18,10 +18,10 @@ export default function ProfileCard(){
             </div>
                 <div className="ProfileRow">
                 <div className="ProfileTextContainer">
-                    <h2><b>Currently Playing:</b> Super Mario Galaxy & Breath Of The Wild & Tomodachi Life LTD</h2>
-                    <h2><b>Currently Watching:</b> The Boys s5 & 2 Broke Girls</h2>
+                    <h2><b>Currently Playing:</b> Lego Marvel Super Heroes (100% run)</h2>
+                    <h2><b>Currently Watching:</b> The Boys s5 & Arrested Development</h2>
                     <h2><b>Song On Repeat:</b> Theme from Superman - John Murphy</h2>
-                    <h2><b>Status:</b> Very excited with all this free time</h2>
+                    <h2><b>Status:</b> Finally coding again</h2>
                     <h2><b>Learning:</b> Unity</h2>
                 </div>
                 <img src="/assets/ProfilePics/PicOFMe.jpg"></img>
