d35e859341d82440c64c42a59d3b67e6663b8dc6
JMWolde <jm.wolde@gmail.com>
2026-07-15 10:35:55 -0600

    minor profile changes (error)

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -18,12 +18,12 @@ export default function ProfileCard(){
             </div>
                 <div className="ProfileRow">
                     <div className="ProfileTextContainer">
-                        <h2><b>Currently Playing:</b> Forza Horizon 6 & New Lego Batman</h2>
+                        <h2><b>Currently Playing:</b> RDR1, GTA SAN AD, GTA 4</h2>
                         <h2><b>Currently Watching:</b> Smallville</h2>
-                        <h2><b>Currently Reading:</b> All-Star Superman</h2>
+                        <h2><b>Currently Reading:</b> Full Metal Alchemist vol 1</h2>
                         <h2><b>Song On Repeat:</b> ydoifeel? - Saam Sultan</h2>
-                        <h2><b>Status:</b> Bored af</h2>
-                        <h2><b>Learning:</b> Nothing atm :(</h2>
+                        <h2><b>Status:</b> Nobody is responding to my messages</h2>
+                        <h2><b>Learning:</b> Drawing the effect, not the object</h2>
                     </div>
                     <img src="/assets/ProfilePics/PicOFMe.jpg"></img>
                 </div>
