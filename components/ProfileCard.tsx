b65ccfb2eb8c3538c4410ddb6cb5ed4ddcea7515
JMWolde <jm.wolde@gmail.com>
2026-08-07 11:00:56 -0600

    minor profile changes

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -18,12 +18,12 @@ export default function ProfileCard(){
             </div>
                 <div className="ProfileRow">
                     <div className="ProfileTextContainer">
-                        <h2><b>Currently Playing:</b> RDR1, GTA SAN AD, GTA 4</h2>
+                        <h2><b>Currently Playing:</b> Hollow Knight</h2>
                         <h2><b>Currently Watching:</b> Smallville</h2>
-                        <h2><b>Currently Reading:</b> Full Metal Alchemist vol 1</h2>
-                        <h2><b>Song On Repeat:</b> ydoifeel? - Saam Sultan</h2>
-                        <h2><b>Status:</b> Nobody is responding to my messages</h2>
-                        <h2><b>Learning:</b> Drawing the effect, not the object</h2>
+                        <h2><b>Currently Reading:</b> The Dark Knight Returns</h2>
+                        <h2><b>Song On Repeat:</b> oh yeah - Steve Lacy</h2>
+                        <h2><b>Status:</b> Indigo is taking all my money</h2>
+                        <h2><b>Learning:</b> Facial Perspectives</h2>
                     </div>
                     <img src="/assets/ProfilePics/PicOFMe.jpg"></img>
                 </div>
